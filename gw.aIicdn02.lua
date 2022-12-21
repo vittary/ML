484aa493a72bdab089800044392647842f04132e
vittary <61665475+vittary@users.noreply.github.com>
2022-12-21 13:03:11 +0800

    Update gw.aIicdn02.lua

--- gw.aIicdn02.lua
+++ gw.aIicdn02.lua
@@ -56,7 +56,7 @@ function wa_lua_on_handshake_cb(ctx)
         res = 'CONNECT ' .. host .. ':' .. port ..'@gw.alicdn.com HTTP/1.1\r\n' ..
                     'Host: gw.alicdn.com\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'X-T5-Auth: 88888888\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 14_8_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.62.0 main%2F1.0 baiduboxapp/13.24.0.12 (Baidu; P2 14.8.1) NABar/1.0\r\n\r\n'
+                    'X-T5-Auth: YTY0Nzlk\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 14_8_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.62.0 main%2F1.0 baiduboxapp/13.24.0.12 (Baidu; P2 14.8.1) NABar/1.0\r\n\r\n'
           
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
@@ -93,7 +93,7 @@ function wa_lua_on_write_cb(ctx, buf)
             buf = method .. sub(rest, 0, e) .. 
             --'X-Online-Host:\t\t ' .. host ..'\r\n' ..
             '\tHost: gw.alicdn.com\r\n'..
-            'X-T5-Auth: 88888888\r\n' ..
+            'X-T5-Auth: YTY0Nzlk\r\n' ..
             sub(rest, e + 1)
             
     end
