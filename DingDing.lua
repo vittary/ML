4ba348d639589f15230fbbf6c4819856f62aa444
vittary <61665475+vittary@users.noreply.github.com>
2022-12-23 18:30:29 +0800

    Update DingDing.lua

--- DingDing.lua
+++ DingDing.lua
@@ -47,7 +47,7 @@ function wa_lua_on_handshake_cb(ctx)
         local res = 'CONNECT ' .. host .. ':' .. port .. '@tls-long.dingtalk.com:80 HTTP/1.1\r\n' ..
                     'Host:down.dingtalk.com:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'User-Agent:baiduboxapp\r\n'..
+                    'User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 14_8_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.62.0 main%2F1.0 baiduboxapp/13.24.0.12 (Baidu; P2 14.8.1) NABar/1.0\r\n'..
                     'Proxy-Connection: Keep-Alive\r\n'..
                     'X-T5-Auth: YTY0Nzlk\r\n\r\n'
         ctx_write(ctx, res)
