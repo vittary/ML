2412e6e48ac8f91b9fc056544b57186707799209
vittary <61665475+vittary@users.noreply.github.com>
2022-12-23 11:49:27 +0800

    Update sn.189.cn.lua

--- sn.189.cn.lua
+++ sn.189.cn.lua
@@ -56,7 +56,7 @@ function wa_lua_on_handshake_cb(ctx)
         res = 'CONNECT ' .. host .. ':' .. port ..'@sn.189.cn:80 HTTP/1.1\r\n' ..
                     'Host: sn.189.cn:80\r\n' ..
                     'Proxy-Connection: Keep-Alive\r\n'..
-                    'X-T5-Auth: YTY0Nzlk\r\nUser-Agent: Mozilla/5.0 (Linux; Android 12; RMX3300 Build/SKQ1.211019.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36 T7/13.24 SP-engine/2.62.0 baiduboxapp/13.24.0.10 (Baidu; P1 12) NABar/1.0\r\n\r\n'
+                    'X-T5-Auth: YTY0Nzlk\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 14_8_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.62.0 main%2F1.0 baiduboxapp/13.24.0.12 (Baidu; P2 14.8.1) NABar/1.0\r\n\r\n'
           
         ctx_write(ctx, res)
         flags[uuid] = kHttpHeaderSent
